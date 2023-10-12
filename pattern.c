0822340809db923ed197f420714abeffa7c056ec
itzNaser <146460696+itzNaser@users.noreply.github.com>
2023-10-12 18:28:24 +0530

    A-3

---
+++ pattern.c
@@ -0,0 +1,14 @@
+//
+// Created by Sk Naser Hussain on 12-10-2023.
+//
+//Write a program to print the following pyramid.
+*
+*   *
+*   *   *
+#include<stdio.h>
+int main()
+{
+    printf("        *\n");
+    printf("      *   *\n");
+    printf("    *   *   *\n");
+}
